db6d405499b70945c4bbdd0d9cdb081d64766255
Marcello <bulletmaster560@gmail.com>
2026-05-31 20:53:22 +0200

    fix: Add TypeScript declarations for react-grid-layout
    - Create types/react-grid-layout.d.ts with module declarations
    - Fixes TypeScript compilation error for GridLayout import
    - Includes types for Layout interface and GridLayoutProps

---
+++ types/react-grid-layout.d.ts
@@ -0,0 +1,40 @@
+declare module 'react-grid-layout' {
+  import React from 'react';
+
+  interface Layout {
+    x: number;
+    y: number;
+    w: number;
+    h: number;
+    i: string;
+    minW?: number;
+    minH?: number;
+    maxW?: number;
+    maxH?: number;
+    static?: boolean;
+    [key: string]: any;
+  }
+
+  interface GridLayoutProps {
+    layout: Layout[];
+    cols?: number;
+    rowHeight?: number;
+    width?: number;
+    isDraggable?: boolean;
+    isResizable?: boolean;
+    isBounded?: boolean;
+    preventCollision?: boolean;
+    compactType?: 'vertical' | 'horizontal' | null;
+    onLayoutChange?: (layout: Layout[]) => void;
+    [key: string]: any;
+  }
+
+  export default class GridLayout extends React.Component<GridLayoutProps> {}
+}
+
+declare module 'react-resizable' {
+  import React from 'react';
+
+  export class ResizeHandle extends React.Component<any> {}
+  export class Resizable extends React.Component<any> {}
+}
